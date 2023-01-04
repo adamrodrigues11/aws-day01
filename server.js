8b34d70fc8751c2de1ee8a25d3e7ae24d91d03bf
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-01-04 15:20:43 -0800

    added a get and post request

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const quotes = [
+    {
+      "quote": "The best preparation for tomorrow is doing your best today.",
+      "author": "H. Jackson Brown, Jr."
+    },
+    {
+      "quote": "The supreme art of war is to subdue the enemy without fighting.",
+      "author": "Sun Tzu"
+    },
+    {
+      "quote": "The journey of a thousand miles begins with one step.",
+      "author": "Lao Tzu"
+    },
+    {
+      "quote": "The World is my country, all mankind are my brethren, and to do good is my religion.",
+      "author": "Thomas Paine"
+    }
+]
+
 app.get("/", (req, res) => {
   console.log("GET /")
   res.send("<h1>This is my first server running on AWS</h1>")
+})
+
+app.get("/api/quotes", (req, res) => {
+    console.log("GET /api/quotes")
+    res.send({quotes: quotes})
+})
+
+app.post("/api/quotes", (req, res) => {
+    const newQuote = req.body
+    console.log("POST /api/quotes", data)
+    quotes.push(newQuote)
+    res.send(newQuote)
 })
 
 const port = process.env.PORT || 8080
