4bd4f523723b2a0a292367c75fc0d83d1fe71cac
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-01-09 18:10:12 -0800

    fixed up the get and post requests using axios

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("build"))
 
 const quotes = [
     {
+      "id": 0,
       "quote": "The best preparation for tomorrow is doing your best today.",
       "author": "H. Jackson Brown, Jr."
     },
     {
+      "id": 1,
       "quote": "The supreme art of war is to subdue the enemy without fighting.",
       "author": "Sun Tzu"
     },
     {
+      "id": 2,
       "quote": "The journey of a thousand miles begins with one step.",
       "author": "Lao Tzu"
     },
     {
+      "id": 3,
       "quote": "The World is my country, all mankind are my brethren, and to do good is my religion.",
       "author": "Thomas Paine"
     }
 ]
-
-app.get("/", (req, res) => {
-  console.log("GET /")
-  res.send("<h1>This is my first server running on AWS</h1>")
-})
 
 app.get("/api/quotes", (req, res) => {
     console.log("GET /api/quotes")
@@ -33,10 +36,19 @@ app.get("/api/quotes", (req, res) => {
 })
 
 app.post("/api/quotes", (req, res) => {
-    const newQuote = req.body
-    console.log("POST /api/quotes", data)
+    const newQuote = {
+      id: quotes.length,
+      quote: req.body.quote,
+      author: req.body.author,
+    }
+    console.log("POST /api/quotes")
+    console.log("new quote", newQuote)
     quotes.push(newQuote)
-    res.send(newQuote)
+    res.send({newQuote: newQuote})
+})
+
+app.get('*', (req, res) => {
+  res.sendFile('build/index.html')
 })
 
 const port = process.env.PORT || 8080
